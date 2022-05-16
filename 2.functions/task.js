1cd485207d3a372c42ebf9101a819ad69fcb1654
Giru <yagofarov.vadim@mail.ru>
2022-05-17 01:16:36 +0500

    add task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,36 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+	let min, max, sum, avg;
+	
+	min = Math.min.apply(null, arr)
+	max = Math.max.apply(null, arr)
+	sum = arr.map(i => x += i, x = 0).reverse()[0]
+	
+	avg = +(sum / arr.length).toFixed(2)
+	return {min: min, max: max, avg: avg};
 }
+
+console.log(getArrayParams([1, 2, 3, -100, 10]))
 
 // Задание 2
 function worker(arr) {
-  let sum;
+	let sum;
 
-  // Ваш код
+	// Ваш код
 
-  return sum;
+	return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+	let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+	// Ваш кода
+	// for ...
+
+	return max;
 }
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+// // Задание 3
+// function worker2(arr) {
+// 	// Ваш код
+// }
