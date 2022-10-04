7dd177ef574df3c65d1f97140956cccec7f829ef
Вадим Ягофаров <VShYagofarov@1cbit.ru>
2022-10-04 09:10:50 +0500

    fix task 1 for lesson 3 arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  result = (arr1.lenght === arr2.lenght) && arr1.every((elem, index) => {console.log(elem === arr2[index])})
+  result = (arr1.lenght === arr2.lenght) && arr1.every((elem, index) => {elem === arr2[index]})
 
   return result; // boolean
 }
