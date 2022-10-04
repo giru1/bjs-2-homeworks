36763d38a609c06f0d2a221c7d6b74fbfc01b4f3
Вадим Ягофаров <VShYagofarov@1cbit.ru>
2022-10-04 09:10:03 +0500

    lesson 3 arrays add task 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = (arr1.lenght === arr2.lenght) && arr1.every((elem, index) => {console.log(elem === arr2[index])})
 
   return result; // boolean
 }
@@ -13,3 +13,10 @@ function advancedFilter(arr) {
 
   return resultArr; // array
 }
+
+// console.log(compareArrays([8, 9], [6]))
+// console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]))
+// console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]))
+// console.log(compareArrays([1, 2, 3], [2, 3, 1]))
+// console.log(compareArrays([8, 1, 2], [8, 1, 2]))
+// console.log(compareArrays([1, 2, 3], [1, 2, 3]))
