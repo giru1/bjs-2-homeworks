d1510b18cf15973b572cd535ec418ccc0404fe99
Вадим Ягофаров <VShYagofarov@1cbit.ru>
2022-10-06 17:51:03 +0500

    add task object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,52 @@
 function Student(name, gender, age) {
-    // Ваш код
+	
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+        this.marks = [mark]
+    } else {
+		this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){ 
+    this.marks = mark
+    } else {
+		this.marks = [].concat(this.marks, ...mark);
+		// this.marks.push(...mark)
+    }
+}
+
+Student.prototype.getAverage = function () {
+	if (this.marks.length === 0) {
+		return 0
+	} else {
+		return this.marks.reduce((sprev, mark) => sprev + mark, 0)/this.marks.length
+	}
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject
+  delete this.marks
+  this.excluded = reason
+  
+}
+
+let student1 = new Student("Vadim", 'man', 31)
+let student2 = new Student("Stepan", 'man', 45)
+let student3 = new Student("Alexei", 'women', 25)
+
+student1.addMark(5)
+student1.addMarks([1,1,1,1,1])
+student1.getAverage()
+console.log(student1)
+console.log(student1.getAverage())
+console.log(student1.exclude('low grades'))
+console.log(student1)
