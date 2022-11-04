21be7b14df2356b00e11194c0f87bb1120151e05
Giru <yagofarov.vadim@mail.ru>
2022-11-04 17:38:01 +0500

    lesson 7.2

--- 7.async/task.js
+++ 7.async/task.js
@@ -7,8 +7,8 @@ class AlarmClock {
 	addClock(time, callback, id) {
 		if(!id) throw new Error( 'Error: невозможно индетифицировать будильник. Параметр id не передан' )
 		
-		if(this.alarmCollection.includes(id)) {
-			console.error(`Звонок с таким id ${id} уже сушествует`);
+		if(this.alarmCollection.some((item) => item.id === id)) {
+			console.error('Звонок с таким id ${id} уже сушествует');
 			return;
 		}
 		
@@ -17,28 +17,52 @@ class AlarmClock {
 	}
 	
 	removeClock(id) {
-		this.alarmCollection = this.alarmCollection.filter()
+		this.alarmCollection = this.alarmCollection.filter(item => item.id !== id)
 	}
 	
 	getCurrentFormattedTime() {
-		return null
+		let date = new Date();
+		let hours = date.getHours() < 10 ? `${'0' +date.getHours()}` : date.getHours()
+		let min = date.getMinutes() < 10 ? `${'0' +date.getMinutes()}` : date.getMinutes()
+		return `${hours}:${min}`
 	}
 	
 	start() {
 	
+		function checkClock(alarm) {
+			if(alarm.time === this.getCurrentFormattedTime()) alarm.callback();
+		}
+		
+		if(!this.timerId) this.timerId = setInterval(()=>this.alarmCollection.some(item => item.checkClock()), 1000)
 	}
 	
 	stop() {
-	
+		if(this.timerId) {
+			clearInterval(this.timerId)
+			this.timerId = null
+		}
 	}
 	
 	printAlarms() {
-	
+		this.alarmCollection.forEach(alarm => console.log(`id ${alarm.id} time ${alarm.time}`))
 	}
 	
 	clearAlarms() {
-	
+		this.stop();
+		this.alarmCollection = [];
 	}
 }
 
-alarm = new AlarmClock()
+clock = new AlarmClock()
+clock.addClock("16:45", f => f, 1);
+clock.addClock("16:45", f => f, 2);
+clock.removeClock(1);
+console.log(clock);
+console.log(clock.getCurrentFormattedTime());
+console.log(clock.printAlarms());
+
+
+clock.start();
+clock.stop();
+
+// clock.clearAlarms();
