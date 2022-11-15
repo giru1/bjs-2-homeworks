98583d0a420ae48ebd785fc76419b4fe7b9a1a43
Giru <yagofarov.vadim@mail.ru>
2022-11-15 08:23:37 +0500

    lesson task 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,31 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+	
+	function wrapper(...args) {
+		const hash = args.join(',');
+		const idx = cache.findIndex( (item) => item.hash === hash )
+		if(idx !== -1) {
+			console.log("Из кэша: " + cache[idx].result);
+			return "Из кэша: " + cache[idx].result;
+		}
+		let result = func(...args);
+		cache.push({hash, result});
+		
+		if (cache.length > 5) {
+			cache.push({hash, result});
+		}
+		
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+	// Ваш код
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+	// Ваш код
 }
